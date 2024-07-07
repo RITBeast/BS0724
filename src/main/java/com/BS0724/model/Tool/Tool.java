0d3690e7b434b86d34a0896565d60cc1403638ae
Brendan <shaw.brendan@gmail.com>
2024-07-07 15:22:26 -0400

    Creation of project, added Data Model and example data for testing.

--- src/main/java/com/BS0724/model/Tool/Tool.java
+++ src/main/java/com/BS0724/model/Tool/Tool.java
@@ -1,9 +1,6 @@
 package com.BS0724.model.Tool;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
